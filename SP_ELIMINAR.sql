31b2ad6ec42f192c7fb6132e4cd498534913e5ee
tatimoramen <tatimorales02@icloud.com>
2020-09-03 15:17:05 -0600

    sp_eliminar medico

--- SP_ELIMINAR.sql
+++ SP_ELIMINAR.sql
@@ -24,26 +24,49 @@ AS
 				PRINT 'EL DNI_PERSONA NO EXISTE'
 			END
 GO
------------------------------ELIMINAR MEDICO-----------------------------------------------------------------
+-----------------------------ELIMINAR Paciente-----------------------------------------------------------------
 USE	GUANA_HOSPI
 GO
-CREATE PROC SP_EliminarPACIENTE (@id_paciente INT)
+CREATE PROC SP_EliminarPaciente (@id_paciente INT)
 AS
 	IF (@id_paciente = '') 
 		BEGIN
-			PRINT 'EL ID DE DE PACIENTE NO PUEDE SER VACÍO'
+			PRINT 'EL ID DE COCINERO NO PUEDE SER VACIO'
 		END
-	ELSE IF (ISNUMERIC(@id_paciente) = 0) 
+	ELSE IF EXISTS (SELECT @id_paciente  FROM Paciente WHERE Paciente.id_paciente = @id_paciente )
 		BEGIN
-			PRINT 'EL ID DE DE PACIENTE NO PUEDE CONTENER CARACTERES'
-		END
-	ELSE IF EXISTS (SELECT id_paciente FROM Paciente WHERE Paciente.id_paciente = @id_paciente)
-		BEGIN
-			DELETE FROM Paciente WHERE Paciente.id_paciente = @id_paciente
-			PRINT 'SE HA ELIMINADO EL PACIENTE'
+		    DELETE FROM Persona WHERE Persona.dni_persona = @id_paciente 
+			PRINT 'SE HA ELIMINADO EL PERSONA'
+			DELETE FROM Paciente WHERE Paciente.id_paciente = @id_paciente 
+			PRINT 'SE HA ELIMINADO EN PACIENTE'
 		END
 	ELSE
 		BEGIN
-			PRINT 'EL ID PACIENTE NO EXISTE'
+			PRINT 'EL PACIENTE NO EXISTE'
 		END
 GO
+-------------------------------------------------Eliminar Medico------------------------------------------------------------------
+USE	GUANA_HOSPI
+GO
+CREATE PROC SP_EliminarMedico (@id_medico INT)
+AS
+	IF (@id_medico = '') 
+		BEGIN
+			PRINT 'EL ID DE COCINERO NO PUEDE SER VACIO'
+		END
+	ELSE IF EXISTS (SELECT @id_medico FROM Medico WHERE Medico.id_medico = @id_medico )
+		BEGIN
+		    DELETE FROM Persona WHERE Persona.dni_persona = @id_medico
+			PRINT 'SE HA ELIMINADO EL PERSONA'
+			DELETE FROM Medico WHERE Medico.id_medico = @id_medico
+			PRINT 'SE HA ELIMINADO EN MEDICO'
+		END
+	ELSE
+		BEGIN
+			PRINT 'EL MEDICO NO EXISTE'
+		END
+GO
+
+drop proc SP_EliminarMedico
+drop proc SP_EliminarPaciente
+
