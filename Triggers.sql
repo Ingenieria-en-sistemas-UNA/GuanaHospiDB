d0a2974bfdeb4a74a8fa1352a2c771b237dda106
itsRicmor <ricardojms1999@gmail.com>
2020-11-02 17:18:04 -0600

    Fix trigger

--- Triggers.sql
+++ Triggers.sql
@@ -4,10 +4,12 @@ GO
 CREATE TRIGGER TR_Insertar_Medico
 ON Medico AFTER INSERT
  AS
-     DECLARE @Id_Usuario VARCHAR(MAX)
-	 DECLARE @Email VARCHAR(MAX)
-	 SELECT @Id_Usuario = CAST(CONTEXT_INFO() AS VARCHAR(MAX))
+     DECLARE @Id_Usuario NVARCHAR
+	 DECLARE @Email NVARCHAR(MAX)
+	 SELECT @Id_Usuario = CAST(CONTEXT_INFO() AS NVARCHAR)
 	 SELECT @Email = email FROM users WHERE id = CONVERT(INT, @Id_Usuario)
 	 INSERT INTO Auditoria (Usuario, Fecha, Descripcion) VALUES (@Email, GETDATE(), 'Ha creado un medico!!')
 
 GO
+
+-- DROP TRIGGER TR_Insertar_Medico
