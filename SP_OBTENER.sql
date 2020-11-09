f15fadcb28034816fbf571294ae6e88a2d5e64ac
Luis <luis.rodriguez.baltodano@est.una.ac.cr>
2020-11-08 23:03:45 -0600

    ..

--- SP_OBTENER.sql
+++ SP_OBTENER.sql
@@ -32,15 +32,6 @@ AS
 	FROM Paciente
 	INNER JOIN Persona
 	ON Persona.dni_persona = Paciente.dni_persona
-GO
-
-CREATE PROC SP_Obtener_Pacientes_Activos
-AS
-	SELECT 'Id_Paciente' = id_paciente, 'Numero_Seguro_Social' = numero_seguro_social , 'Fecha_Ingreso' = fecha_ingreso, 'Cedula_Persona' = Paciente.dni_persona, 'Nombre_Persona' = nombre_persona, 'Primer_Apellido' = apellido_1,
-	'Segundo_Apellido' = apellido_2, ok = 1
-	FROM Paciente
-	INNER JOIN Persona
-	ON Persona.dni_persona = Paciente.dni_persona
 	WHERE estado_paciente = 1
 GO
 
@@ -93,14 +84,6 @@ AS
 GO
 
 CREATE PROC SP_Obtener_Medicos
-AS
-	SELECT 'Id_Medico' = id_medico, 'Codigo_Medico' = codigo_medico , 'Cedula_Persona' = Medico.dni_persona, 'Nombre_Persona' = nombre_persona, 'Primer_Apellido' = apellido_1,
-	'Segundo_Apellido' = apellido_2, 'Edad' = edad, 'Estado' = estado ,ok = 1	FROM Medico
-	INNER JOIN Persona
-	ON Persona.dni_persona = Medico.dni_persona
-GO
-
-CREATE PROC SP_Obtener_Medicos_Activos
 AS
 	SELECT 'Id_Medico' = id_medico, 'Codigo_Medico' = codigo_medico , 'Cedula_Persona' = Medico.dni_persona, 'Nombre_Persona' = nombre_persona, 'Primer_Apellido' = apellido_1,
 	'Segundo_Apellido' = apellido_2, 'Edad' = edad, 'Estado' = estado ,ok = 1	FROM Medico
@@ -177,13 +160,6 @@ AS
 GO
 
 CREATE PROC SP_Obtener_Consultas
-AS
-	SELECT 'Id_Consulta' = id_consulta, 'Fehca_Consulta' = fecha_consulta , descripcion , 'Id_Paciente' = id_paciente, 
-	'Id_Medico' = id_medico, 'Id_Unidad' = id_unidad ,ok = 1
-	FROM Consulta
-GO
-
-CREATE PROC SP_Obtener_Consultas_Activas
 AS
 	SELECT 'Id_Consulta' = id_consulta, 'Fehca_Consulta' = fecha_consulta , descripcion , 'Id_Paciente' = id_paciente, 
 	'Id_Medico' = id_medico, 'Id_Unidad' = id_unidad ,'Estado_Consulta'=estado_consulta,ok = 1
