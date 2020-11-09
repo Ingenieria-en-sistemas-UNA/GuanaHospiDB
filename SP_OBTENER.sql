f480577b764722891e1999271c30489aee5bd087
Luis <luis.rodriguez.baltodano@est.una.ac.cr>
2020-11-08 23:40:39 -0600

    ..

--- SP_OBTENER.sql
+++ SP_OBTENER.sql
@@ -6,6 +6,7 @@ AS
 	SELECT 'Cedula_Persona' = dni_persona , 'Nombre_Persona' = nombre_persona, 'Primer_Apellido' = apellido_1,
 		'Segundo_Apellido' = apellido_2, 'Edad' = edad, ok = 1
 	FROM Persona
+	ORDER BY dni_persona ASC;
 GO
 
 CREATE PROC SP_Obtener_Persona_Por_Dni
@@ -20,7 +21,8 @@ AS
 			SELECT 'Cedula_Persona' = dni_persona , 'Nombre_Persona' = nombre_persona, 'Primer_Apellido' = apellido_1,
 				'Segundo_Apellido' = apellido_2, 'Edad' = edad, ok = 1
 			FROM Persona
-			WHERE dni_persona = @dni_persona;
+			WHERE dni_persona = @dni_persona
+			ORDER BY dni_persona ASC;
 		END
 GO
 
@@ -33,6 +35,7 @@ AS
 	INNER JOIN Persona
 	ON Persona.dni_persona = Paciente.dni_persona
 	WHERE estado_paciente = 1
+	ORDER BY id_paciente ASC;
 GO
 
 CREATE PROC SP_Obtener_Paciente_Por_Id
@@ -53,7 +56,8 @@ AS
 			FROM Paciente
 			INNER JOIN Persona
 			ON Persona.dni_persona = Paciente.dni_persona
-			WHERE id_paciente = @id_paciente;
+			WHERE id_paciente = @id_paciente
+			ORDER BY id_paciente ASC;
 		END
 GO
 
@@ -62,6 +66,7 @@ CREATE PROC SP_Obtener_Unidades
 AS
 	SELECT 'Id_Unidad' = id_unidad, 'Nombre_Unidad' = nombre_unidad , 'Numero_Planta' = numero_planta, ok = 1
 	FROM Unidad
+	ORDER BY id_unidad ASC;
 GO
 
 CREATE PROC SP_Obtener_Unidade_Por_Id
@@ -79,7 +84,8 @@ AS
 		BEGIN
 			SELECT 'Id_Unidad' = id_unidad, 'Nombre_Unidad' = nombre_unidad , 'Numero_Planta' = numero_planta, 'Id_Medico' = id_medico, ok = 1
 			FROM Unidad
-			WHERE id_unidad = @id_unidad;
+			WHERE id_unidad = @id_unidad
+			ORDER BY id_unidad ASC;
 		END
 GO
 
@@ -90,6 +96,7 @@ AS
 	INNER JOIN Persona
 	ON Persona.dni_persona = Medico.dni_persona
 	WHERE estado = 1
+	ORDER BY id_medico ASC;
 GO
 
 
@@ -111,7 +118,8 @@ AS
 			FROM Medico
 			INNER JOIN Persona
 			ON Persona.dni_persona = Medico.dni_persona
-			WHERE id_medico = @id_medico;
+			WHERE id_medico = @id_medico
+			ORDER BY id_medico ASC;
 		END
 GO
 
@@ -119,6 +127,7 @@ CREATE PROC SP_Obtener_Roles
 AS
 	SELECT 'Id_Role' = id_role, 'Nombre_Role' = nombre_role, ok = 1
 	FROM Roles
+	ORDER BY id_role ASC;
 GO
 
 CREATE PROC SP_Obtener_Role_Por_ID
@@ -137,6 +146,7 @@ AS
 			SELECT 'Id_Role' = id_role, 'Nombre_Role' = nombre_role, ok = 1
 			FROM Roles
 			WHERE id_role = @IdRole
+			ORDER BY id_role ASC;
 		END
 GO
 
@@ -152,6 +162,7 @@ AS
 			SELECT 'Id_Role' = id_role, 'Nombre_Role' = nombre_role, ok = 1
 			FROM Roles
 			WHERE nombre_role = @NombreRole
+			ORDER BY id_role ASC;
 		END
 	ELSE
 		BEGIN
@@ -165,6 +176,7 @@ AS
 	'Id_Medico' = id_medico, 'Id_Unidad' = id_unidad ,'Estado_Consulta'=estado_consulta,ok = 1
 	FROM Consulta
 	WHERE estado_consulta = 1
+	ORDER BY id_consulta ASC;
 GO
 
 
@@ -175,6 +187,7 @@ AS
 	'Id_Medico' = id_medico, 'Id_Unidad' = id_unidad ,ok = 1
 	FROM Consulta
 	WHERE id_paciente = @Id_paciente 
+	ORDER BY id_consulta ASC;
 GO
 
 USE GUANA_HOSPI
@@ -197,7 +210,8 @@ AS
 			FROM Consulta 
 			INNER JOIN Paciente ON Consulta.id_paciente = Paciente.id_paciente
 			INNER JOIN Persona ON Persona.dni_persona = Paciente.dni_persona
-			WHERE id_consulta = @id_consulta;
+			WHERE id_consulta = @id_consulta
+			ORDER BY id_consulta ASC;
 		END
 GO
 
@@ -206,6 +220,7 @@ CREATE PROC SP_Obtener_Enfermedades
 AS
 	SELECT 'Id_Enfermedad' = id_enfermedad, 'Nombre_Enfermedad' = nombre_enfermedad, ok = 1
 	FROM Enfermedad
+	ORDER BY id_enfermedad ASC;
 GO
 
 CREATE PROCEDURE SP_Obtener_Enfermedades_Por_Id_Paciente_Y_Consulta_Id
@@ -232,6 +247,7 @@ AS
 			SELECT 'Id_Enfermedad' = Enfermedad.id_enfermedad, 'Nombre_Enfermedad' = Enfermedad.nombre_enfermedad, 'Id_Consulta' = Padece.id_consulta, ok = 1
 			FROM Enfermedad INNER JOIN Padece ON Enfermedad.id_enfermedad = Padece.id_enfermedad
 			WHERE id_paciente = @id_paciente AND id_consulta = @id_consulta
+			ORDER BY id_enfermedad ASC;
 		END
 GO
 
@@ -250,7 +266,8 @@ AS
 		BEGIN
 			SELECT 'Id_Enfermedad' = id_enfermedad, 'Nombre_Enfermedad' = nombre_enfermedad, ok = 1
 			FROM Enfermedad
-			WHERE id_enfermedad = @id_enfermedad;
+			WHERE id_enfermedad = @id_enfermedad
+			ORDER BY id_enfermedad ASC;
 		END
 GO
 
@@ -258,6 +275,7 @@ CREATE PROC SP_Obtener_Intervenciones
 AS
 	SELECT 'Id_Intervencion' = id_intervencion, 'Tratamiento' = tratamiento, 'Id_Tipo_Intervencion' = id_tipo_intervencion, 'Id_Consulta' = id_consulta, ok = 1
 	FROM Intervenciones
+	ORDER BY id_intervencion ASC;
 GO
 
 CREATE PROC SP_Obtener_Interv_Por_Id_Consulta
@@ -276,7 +294,8 @@ AS
 		SELECT 'Id_Intervencion' = id_intervencion, 'Tratamiento' = tratamiento, 'Id_Tipo_Intervencion' = Intervenciones.id_tipo_intervencion, 'Id_Consulta' = id_consulta, 'Nombre_Tipo_Intervencion' = Tipo_Intervencion.nombre_tipo_intervencion, ok = 1
 		FROM Intervenciones
 		INNER JOIN Tipo_Intervencion ON Tipo_Intervencion.id_tipo_intervencion = Intervenciones.id_tipo_intervencion
-		WHERE id_consulta = @id_consulta;
+		WHERE id_consulta = @id_consulta
+		ORDER BY id_intervencion ASC;
 	END
 GO
 
@@ -296,7 +315,8 @@ AS
 		BEGIN
 			SELECT 'Id_Intervencion' = id_intervencion, 'Tratamiento' = tratamiento, 'Id_Tipo_Intervencion' = id_tipo_intervencion, 'Id_Consulta' = id_consulta, ok = 1
 			FROM Intervenciones
-			WHERE id_intervencion = @id_intervencion;
+			WHERE id_intervencion = @id_intervencion
+			ORDER BY id_intervencion ASC;
 		END
 GO
 
@@ -304,6 +324,7 @@ CREATE PROC SP_Obtener_Especialidades
 AS
 	SELECT 'Id_Especialidad' = id_especialidad, 'Nombre_Especialidad' = nombre_especialdad, ok = 1
 	FROM Especialidad
+	ORDER BY id_especialidad ASC;
 GO
 
 CREATE PROC SP_Obtener_Especialidades_Por_Id
@@ -321,7 +342,8 @@ AS
 		BEGIN
 			SELECT 'Id_Especialidad' = id_especialidad, 'Nombre_Especialidad' = nombre_especialdad, ok = 1
 			FROM Especialidad
-			WHERE id_especialidad = @id_especialidad;
+			WHERE id_especialidad = @id_especialidad
+			ORDER BY id_especialidad ASC;
 		END
 GO
 
@@ -345,7 +367,8 @@ AS
 			SELECT 'Id_Especialidad' = e.id_especialidad, 'Nombre_Especialidad' = e.nombre_especialdad, ok = 1
 			FROM Especialidad e
 			INNER JOIN Medico_Especialidad me ON e.id_especialidad = me.id_especialidad
-			WHERE me.id_medico = @id_medico;
+			WHERE me.id_medico = @id_medico
+			ORDER BY e.id_especialidad ASC;
 		END
 GO
 
@@ -353,6 +376,7 @@ CREATE PROC SP_Obtener_Padece
 AS
 	SELECT 'Id_Padece' = id_padece, 'Id_Paciente' = id_paciente, 'Id_Enfermedad' = id_enfermedad, 'Id_Consulta' = id_consulta, ok = 1
 	FROM Padece
+	ORDER BY id_padece ASC;
 GO
 
 CREATE PROC SP_Obtener_Padece_Por_Id
@@ -370,7 +394,8 @@ AS
 		BEGIN
 			SELECT 'Id_Padece' = id_padece, 'Id_Paciente' = id_paciente, 'Id_Enfermedad' = id_enfermedad,'Id_Consulta' id_consulta,ok = 1
 			FROM Padece
-			WHERE id_padece = @id_padece;
+			WHERE id_padece = @id_padece
+			ORDER BY id_padece ASC;
 		END
 GO
 
@@ -378,6 +403,7 @@ CREATE PROC SP_Obtener_Tipos_Intervenciones
 AS
 	SELECT 'Id_Tipo_Intervencion' = id_tipo_intervencion, 'Nombre_Tipo_Intervencion' = nombre_tipo_intervencion, ok = 1
 	FROM Tipo_Intervencion
+	ORDER BY id_tipo_intervencion ASC;
 GO
 
 CREATE PROC SP_Obtener_Tipo_Intervencion_Por_Id_Consulta
@@ -395,7 +421,8 @@ AS
 	BEGIN
 		SELECT 'Id_Tipo_Intervencion' = Tipo_Intervencion.id_tipo_intervencion, 'Nombre_Tipo_Intervencion' = Tipo_Intervencion.nombre_tipo_intervencion, ok = 1
 		FROM Tipo_Intervencion INNER JOIN Intervenciones ON Tipo_Intervencion.id_tipo_intervencion = Intervenciones.id_tipo_intervencion
-		WHERE id_consulta = @id_consulta;
+		WHERE id_consulta = @id_consulta
+		ORDER BY id_tipo_intervencion ASC;
 	END
 GO
 
@@ -414,7 +441,8 @@ AS
 		BEGIN
 			SELECT 'Id_Tipo_Intervencion' = id_tipo_intervencion, 'Nombre_Tipo_Intervencion' = nombre_tipo_intervencion, ok = 1
 			FROM Tipo_Intervencion
-			WHERE id_tipo_intervencion = @id_tipo_intervencion;
+			WHERE id_tipo_intervencion = @id_tipo_intervencion
+			ORDER BY id_tipo_intervencion ASC;
 		END
 GO
 
@@ -422,6 +450,7 @@ CREATE PROC SP_Obtener_Medico_Especialidad
 AS
 	SELECT 'Id_Medico_Especialidad' = id_medico_especialidad, 'Id_Medico' = id_medico, 'Id_Especialidad' = id_especialidad, ok = 1
 	FROM Medico_Especialidad
+	ORDER BY id_medico_especialidad ASC;
 GO
 
 USE GUANA_HOSPI
@@ -441,7 +470,8 @@ AS
 		BEGIN
 			SELECT 'Id_Medico_Especialidad' = id_medico_especialidad, 'Id_Medico' = id_medico, 'Id_Especialidad' = id_especialidad, ok = 1
 			FROM Medico_Especialidad
-			WHERE id_medico = @id_medico;
+			WHERE id_medico = @id_medico
+			ORDER BY id_medico_especialidad ASC;
 		END
 GO
 
@@ -449,4 +479,5 @@ CREATE PROC SP_Obtener_Auditoria
 AS
 	SELECT 'Email' = Usuario, 'Fecha' = Fecha, 'Accion' = Descripcion, ok = 1
 	FROM Auditoria
+	ORDER BY Fecha ASC;
 GO
