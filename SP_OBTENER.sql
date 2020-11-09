c8c0cb05128d991bc822042050cd41b9207663ba
Luis <luis.rodriguez.baltodano@est.una.ac.cr>
2020-11-08 18:04:12 -0600

    ..

--- SP_OBTENER.sql
+++ SP_OBTENER.sql
@@ -177,7 +177,7 @@ AS
 	SELECT 'Id_Consulta' = id_consulta, 'Fehca_Consulta' = fecha_consulta , descripcion , 'Id_Paciente' = id_paciente, 
 	'Id_Medico' = id_medico, 'Id_Unidad' = id_unidad ,ok = 1
 	FROM Consulta
-	WHERE id_paciente = @Id_paciente
+	WHERE id_paciente = @Id_paciente 
 GO
 
 USE GUANA_HOSPI
